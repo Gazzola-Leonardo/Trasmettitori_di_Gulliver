e0067ca3efc1c8e9969bfca93a35b7ce7f8cb255
Gazzola Leonardo <leonardo.gazzola@barsanti.edu.it>
2020-01-11 22:45:22 +0100

    Miglioramenti al programma

--- Trasmettitori_di_Gulliver.ino
+++ Trasmettitori_di_Gulliver.ino
@@ -1,9 +1,24 @@
+int led = 13;
+String input;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+  //Serial.println("Inserire la frase da inviare");
+  while(Serial.available()){
+    input = Serial.readString();
+    
+    Serial.println(input);
+    
+    delay(2000);
+    //digitalWrite(led, HIGH);
+  }
+  
 }
