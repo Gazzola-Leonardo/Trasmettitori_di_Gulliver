e086dd4e7e6f106914fcd64ede5fed0a6eadcc81
Gazzola Leonardo <leonardo.gazzola@barsanti.edu.it>
2020-02-13 20:19:14 +0100

    Miglioramento codice

--- Trasmettitori_di_Gulliver.ino
+++ Trasmettitori_di_Gulliver.ino
@@ -2,7 +2,7 @@ int led = 13;
 String input;
 
 char Caratteri[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '•', ',', ':', '?', '=', '-', '(', ')', '"', '/', '@', '!'};
-char Codice[] = {'• —', '— • • •', '— • — •', '— • •', '•', '• • — •', '— — •', '• • • •', '• •', '• — — —', '— • —', '• — • •', '— —', '— •', '— — —', '• — — •', '— — • —', '• — •', '• • •', '—', '• • —', '• • • —', '• — —', '— • • —', '— • — —', '— — • •', '— — — — —', '• — — — —', '• • — — —', '• • • — —', '• • • • —', '• • • • •', '— • • • •', '— — • • •', '— — — • •', '— — — — •', '• — • — • —', '— — • • — —', '— — — • • •', '• • — — • •', '— • • • —', '— • • • • —', '— • — — •', '— • — — • —', '• — • • — •', '— • • — •', '• — — • — •', '— • — • — —'};
+char Codice[5];
 
 int tPunto = 500;
 int tTrattino = 1000;
@@ -21,7 +21,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Codice[2] = "...--";
   Serial.println("scrivi");
   input = Serial.readString(); 
   while(input.length() == 0){
@@ -57,21 +57,66 @@ void loop() {
   //Serial.println(charBuf[1]);
   //Serial.println(lung);
   
+  char app;
   
   for(int i = 0; i < lung - 1; i++){
-    char app = input[i];
+    app = input[i];
     Serial.println("Appoggio: ");
     Serial.println(app);
-    
-    for(int j = 0; j < 47; j++){
-      Serial.println("Entrato nel for");
-      if(app == Caratteri[j]){
-        Serial.println("Trovato");
+
+    if(app == " "){
+      codiceLetteraLuce(posSpazio);
+    }
+    else{
+      for(int j = 0; j < 47; j++){
+        Serial.println(Caratteri[j]);
+        Serial.println(app);
+        
+        if(app == Caratteri[j]){
+          codiceLetteraLuce(j);
+          Serial.println("Trovato");
+          break;
+        }
       }
-    
     }
   }
+  if(app != "."){
+    codiceLetteraLuce(posPunto);
+  }
+  
   input = "";
   
+  
+}
+
+void codiceLetteraLuce(int pos){
+  
+  Serial.println(pos, DEC);
+  char cod = Codice[2];
+
+  Serial.println(cod);
+  Serial.println(cod);
+
+  
+  digitalWrite(led, HIGH);  
+  delay(1000);
+  digitalWrite(led, LOW);  
+  
+/*
+  for(int i = 0; i < cod.length(); i++){
+    Serial.println(cod[i]);
+    if(cod[i] == "."){
+      digitalWrite(led, HIGH);  
+      delay(tPunto);
+    }
+    else if(cod[i] == "-"){
+      digitalWrite(led, HIGH);  
+      delay(tTrattino);
+    }
+    digitalWrite(led, LOW);
+    delay(stacco);
+  }
+  delay(fineCarattere);
+*/
   
 }
